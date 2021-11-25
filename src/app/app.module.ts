bd6e27f4077de560215affe28830d9ec2dfebbd0
tazagroup <chikiet88@gmail.com>
2021-11-25 13:47:19 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,7 @@ const appRoutes: Routes = [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
-        RouterModule.forRoot(appRoutes),
+        RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' }),
 
         TranslateModule.forRoot(),
         InMemoryWebApiModule.forRoot(FakeDbService, {
--- src/app/main/hrm/danhsach/danhsach.component.spec.ts
+++ src/app/main/hrm/danhsach/danhsach.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DanhsachComponent } from './danhsach.component';
 
@@ -6,7 +6,7 @@ describe('DanhsachComponent', () => {
   let component: DanhsachComponent;
   let fixture: ComponentFixture<DanhsachComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DanhsachComponent ]
     })
